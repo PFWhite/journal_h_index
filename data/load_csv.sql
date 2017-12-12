74fd909dd66d390e83e42ed765b8e54324d32ac6
Patrick White <pfwhite@ufl.edu>
2017-12-12 10:40:23 -0500

    fix tables in database, scripts conversion, documents load

---
+++ data/load_csv.sql
@@ -0,0 +1,11 @@
+-- make sure to run the convert script beforehand
+.mode csv
+.import raw_data/scimagojr_2008.csv scimagojr_2008
+.import raw_data/scimagojr_2009.csv scimagojr_2009
+.import raw_data/scimagojr_2010.csv scimagojr_2010
+.import raw_data/scimagojr_2011.csv scimagojr_2011
+.import raw_data/scimagojr_2012.csv scimagojr_2012
+.import raw_data/scimagojr_2013.csv scimagojr_2013
+.import raw_data/scimagojr_2014.csv scimagojr_2014
+.import raw_data/scimagojr_2015.csv scimagojr_2015
+.import raw_data/scimagojr_2016.csv scimagojr_2016
